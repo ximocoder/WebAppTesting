9db03ef6c3e0cd20ff96915bab5179e814bc9739
Ximo <Ximo@DESKTOP>
2022-10-21 21:54:33 +0200

    Init commit

---
+++ Controllers/ExcusesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTesting.Controllers
+{
+    public class ExcusesController : Controller
+    {
+        public IActionResult Index()
+        {
+        
+        }
+    }
+}
